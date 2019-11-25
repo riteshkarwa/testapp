6485531e9600becd7318ff79a3221015baa17b3a
ritesh <ssr@gmail.com>
2019-11-25 13:27:24 -0800

    second commit

--- express/server.js
+++ express/server.js
@@ -20,48 +20,12 @@ app.use(function(req, res, next) {
   next();
 });
 
-// for local testing
-var connection = mysql.createConnection({
-  host : "remotemysql.com",
-  user: "XQu1tDNQ0B",
-  database: "XQu1tDNQ0B",
-  password: "xNwMQh7jQ8"
-  //port:"3306"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('Error connecting: ' + err.stack);
-    return;
-  }
-  console.log('Connected as thread id: ' + connection.threadId);
-
-});
-
-// Query Database to get all likes
-app.get('/api/all_likes', function(req, res) {
-  var results=[];
-  
-  //SQL Query > Select Data
-  connection.query("SELECT * FROM num_of_likes", function(err, rows, fields) {
-    if (err) throw err;
-
-    for (var r in rows){
-      console.log(rows[r]);
-      results.push(rows[r]);
-    } 
-    return res.json(rows);
-  });
-});
 
 console.log(path.resolve(__dirname, '../public'));
 
 app.use(express.static('./public'))
 
-app.all('*', function(request, response, next) {
-  //response.sendFile('index.html', {root: './public'});
-  response.sendFile(path.resolve(__dirname, '../public')+'/index.html');
-});
+
 router.get('/*', function(request, response, next) {
   //response.sendFile('index.html', {root: './public'});
   response.sendFile(path.resolve(__dirname, '../public')+'/index.html');
