1037186e2e28b6eea36164387e1491f9f65d6afb
ritesh <ssr@gmail.com>
2019-11-25 13:52:03 -0800

    created toml file

--- express/server.js
+++ express/server.js
@@ -26,12 +26,12 @@ console.log(path.resolve(__dirname, '../public'));
 app.use(express.static('./public'))
 
 
-router.get('/*', function(request, response, next) {
+router.get('/*', function(request, response) {
   //response.sendFile('index.html', {root: './public'});
   response.sendFile(path.resolve(__dirname, '../public')+'/index.html');
 });
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('*', router);  // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
